fa9b0f66b42c9ac6298d5d219fe8bc5d84858bd8
Pravinkumar2629 <pravinkmr2619@gmail.com>
2021-01-02 19:13:47 +0530

    singleton DI with boost library

---
+++ Singleton/SingletonDI.cpp
@@ -0,0 +1,24 @@
+/*
+ * SingletonDI.cpp
+ *
+ *  Created on: Jan 2, 2021
+ *      Author: pravinkumar
+ */
+
+
+
+struct Company{
+	int _id = 0;
+};
+
+struct Employee{
+	Company *c;
+};
+
+
+int main(){
+
+}
+
+
+
